48a60b301b7c7266ed09819c6623d5622f93b3a3
Ricardo-USC <r_g108@student.usc.edu.au>
2024-06-13 18:49:35 +1000

    Demo Account: Set-up a function that creates a demo account + further documentation

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -2,6 +2,11 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+/**
+ * Hashes a password, to store passwords securely.
+ * @param {string} password- The user's password.
+ * @return {string} A hashed password.
+ */
 async function hashPassword(password) {
   try {
     const hashedPassword = await bcrypt.hash(password, 8);
@@ -13,6 +18,12 @@ async function hashPassword(password) {
   }
 }
 
+/**
+ * Validates a password with it's hashed counterpart.
+ * @param {string} password- The user's password.
+ * @param {string} passwordToMatch- The hashed password to match.
+ * @return {Boolean} True - Passwords match. False - Passwords don't match.
+ */
 async function validPassword(password, passwordToMatch) {
   try {
     const isPasswordMatch = await bcrypt.compare(password, passwordToMatch);
@@ -23,6 +34,11 @@ async function validPassword(password, passwordToMatch) {
   }
 }
 
+/**
+ * Generates a JSON web token to authenticate user's.
+ * @param {Object} userDetails- The user's details.
+ * @return {Object} JSON web token.
+ */
 function generateJWT(userDetails) {
     return jwt.sign(
         { userId: userDetails.recid, email: userDetails.email },
--- api/database/dbConnection.js
+++ api/database/dbConnection.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
+const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
+// Creates a connection pool to the database.
 const sql = new Pool({
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
@@ -9,6 +11,10 @@ const sql = new Pool({
   database: process.env.POSTGRES_DB,
 });
 
+
+/**
+ * Checks the tables for the app.
+ */
 async function checkTables() {
   try {
     await checkTableExists("users", createUsersTable);
@@ -19,6 +25,11 @@ async function checkTables() {
   }
 }
 
+/**
+ * Checks to see if the tables needed for the app exist.
+ * @param {string} tableName- The tables name.
+ * @param {function} createTable - The function to create the relevant table.
+ */
 async function checkTableExists(tableName, createTable) {
   try {
     const result = await sql.query(
@@ -30,6 +41,9 @@ async function checkTableExists(tableName, createTable) {
     if (!tableExists) {
       console.log(`${tableName} table does not exist, creating it now.`);
       await createTable();
+      if (tableName === "exercises") {
+        await createDemoUser();
+      }
     } else {
       console.log(`Table ${tableName} exists:`, tableExists);
     }
@@ -39,6 +53,9 @@ async function checkTableExists(tableName, createTable) {
   }
 }
 
+/**
+ * Creates the users tables.
+ */
 async function createUsersTable() {
   try {
     await sql.query(
@@ -55,6 +72,9 @@ async function createUsersTable() {
   }
 }
 
+/**
+ * Creates the workouts tables.
+ */
 async function createWorkoutsTable() {
   try {
     await sql.query(
@@ -73,6 +93,9 @@ async function createWorkoutsTable() {
   }
 }
 
+/**
+ * Creates the exercises tables.
+ */
 async function createExerciseTable() {
   try {
     await sql.query(
@@ -91,6 +114,102 @@ async function createExerciseTable() {
     console.log("Exercises table created.");
   } catch (err) {
     console.error("Error creating exercises table.", err);
+  }
+}
+
+/**
+ * Creates a demo user.
+ */
+async function createDemoUser() {
+  try {
+    const userId = uuidv4();
+    const pushDayId = uuidv4();
+    const pullDayId = uuidv4();
+    const legDayId = uuidv4();
+    const upperBodyDayId = uuidv4();
+    const lowerBodyDayId = uuidv4();
+
+    await sql.query(
+      `INSERT INTO users (recId, email, password) VALUES
+      ($1, 'demo@user.com', '$2b$08$tDSoPYunn4olGO0VaNFVqOeedgfondpqRi9Enp7zn0xh9Q4TJWqXC');`,
+      [userId]
+    );
+
+    await sql.query(
+      `INSERT INTO workouts (recId, userRecId, workoutName) VALUES
+      ($1, $2, 'Push Day'),
+      ($3, $2, 'Pull Day'),
+      ($4, $2, 'Leg Day'),
+      ($5, $2, 'Upper Body Day'),
+      ($6, $2, 'Lower Body Day');`,
+      [pushDayId, userId, pullDayId, legDayId, upperBodyDayId, lowerBodyDayId]
+    );
+
+    await sql.query(
+      `INSERT INTO exercises (recId, userRecId, workoutRecId, exerciseName, sets) VALUES
+      ($1, $2, $3, 'Bench Press', '3 Sets'),
+      ($4, $2, $3, 'Shoulder Press', '3 Sets'),
+      ($5, $2, $3, 'Tricep Dips', '3 Sets'),
+      ($6, $2, $3, 'Chest Flyes', '3 Sets'),
+      ($7, $2, $3, 'Tricep Pushdowns', '3 Sets'),
+      ($8, $2, $9, 'Pull-Ups', '3 Sets'),
+      ($10, $2, $9, 'Barbell Rows', '3 Sets'),
+      ($11, $2, $9, 'Bicep Curls', '3 Sets'),
+      ($12, $2, $9, 'Face Pulls', '3 Sets'),
+      ($13, $2, $9, 'Lat Pulldowns', '3 Sets'),
+      ($14, $2, $15, 'Squats', '3 Sets'),
+      ($16, $2, $15, 'Leg Press', '3 Sets'),
+      ($17, $2, $15, 'Leg Curls', '3 Sets'),
+      ($18, $2, $15, 'Leg Extensions', '3 Sets'),
+      ($19, $2, $15, 'Calf Raises', '3 Sets'),
+      ($20, $2, $21, 'Incline Bench Press', '3 Sets'),
+      ($22, $2, $21, 'Bent Over Rows', '3 Sets'),
+      ($23, $2, $21, 'Lateral Raises', '3 Sets'),
+      ($24, $2, $21, 'Tricep Extensions', '3 Sets'),
+      ($25, $2, $21, 'Bicep Curls', '3 Sets'),
+      ($26, $2, $27, 'Deadlifts', '3 Sets'),
+      ($28, $2, $27, 'Lunges', '3 Sets'),
+      ($29, $2, $27, 'Hamstring Curls', '3 Sets'),
+      ($30, $2, $27, 'Glute Bridges', '3 Sets'),
+      ($31, $2, $27, 'Calf Raises', '3 Sets');`,
+      [
+        uuidv4(),
+        userId,
+        pushDayId,
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        pullDayId,
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        legDayId,
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        upperBodyDayId,
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        lowerBodyDayId,
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+        uuidv4(),
+      ]
+    );
+
+    console.log("Demo user created.");
+  } catch (err) {
+    console.error("Error creating demo user.", err);
   }
 }
 
